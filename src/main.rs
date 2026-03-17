d798be8550998ebdbf234d989fe83fd456b0be15
Leven Rochana <levenrok@proton.me>
2026-03-17 10:16:14 +0530

    implement rom loading functionality

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,23 @@
-fn main() {}
+use std::env;
+use std::fs;
+
+use anyhow::{Result, bail};
+
+mod rom;
+use rom::Rom;
+
+fn main() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        bail!("specify a rom file to load!");
+    }
+
+    let filename = &args[1];
+
+    let _rom_data: Vec<u8> = fs::read(filename)?;
+
+    println!("loading ROM: \x1b[1m{}\x1b[0m", filename);
+
+    Ok(())
+}
