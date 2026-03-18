7fd766ee8a17ad61c5372b3330d79868c915cda1
Leven Rochana <levenrok@proton.me>
2026-03-18 12:21:24 +0530

    change rust toolchain to stable

--- flake.nix
+++ flake.nix
@@ -4,43 +4,39 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs =
     { self
     , nixpkgs
     , flake-utils
-    , fenix
     , ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = import nixpkgs { inherit system; };
-
-        fenixLib = fenix.packages.${system};
-
-        rustToolchain = fenixLib.latest.toolchain;
       in
       {
         devShells.default = pkgs.mkShell {
           name = "rust";
 
-          nativeBuildInputs = [
-            rustToolchain
+          nativeBuildInputs = with pkgs; [
+            cargo
+            rustc
+            lld
+            mold
 
-            pkgs.clang
-            pkgs.mold
+            rust-analyzer
+            rustfmt
+            clippy
           ];
 
           # buildInputs = with pkgs; [
           #   SDL2
           # ];
+
+          env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
 
           shellHook = ''
             echo -e "\033[0;32mDone!\033[0m"
