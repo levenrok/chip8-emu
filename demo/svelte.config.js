87cffd835cd840c639795b74519bd9a605fb5565
Leven Rochana <levenrok@proton.me>
2026-03-22 12:59:27 +0530

    fix base path for the demo website

--- demo/svelte.config.js
+++ demo/svelte.config.js
@@ -10,6 +10,9 @@ const config = {
             precompress: false,
             strict: true,
         }),
+        paths: {
+            base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
+        },
     },
     vitePlugin: {
         dynamicCompileOptions: ({ filename }) =>
